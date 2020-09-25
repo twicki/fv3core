065e3ceefac534db3340b6a39ad2e297f02e1aee
Rhea George <rheag@vulcan.com>
2020-09-25 16:33:56 -0700

    split testing FVDynamics from the other parallel tests

--- .jenkins/actions/run_parallel_regression_tests.sh
+++ .jenkins/actions/run_parallel_regression_tests.sh
@@ -2,7 +2,7 @@
 set -e -x
 BACKEND=$1
 EXPNAME=$2
-ARGS="-vv -s -rsx --backend=${BACKEND} --junitxml=/.jenkins/parallel_test_results.xml"
+ARGS="-vv -s -rsx --backend=${BACKEND} --which_modules=CubedToLatLon,DynCore,FVSubgridZ,HaloUpdate,HaloUpdate_2,HaloVectorUpdate,MPPBoundaryAdjust,MPPUpdateDomains,Tracer2D1L --junitxml=/.jenkins/parallel_test_results.xml"
 export EXPERIMENT=${EXPNAME}
 
 # Set the host data location
@@ -13,6 +13,9 @@ make get_test_data
 
 # The default of this set to 1 causes a segfault
 make run_tests_parallel TEST_ARGS="${ARGS}"
+
+ARGS_FVDYN="-vv -s -rsx --backend=${BACKEND} --which_modules=FVDynamics --junitxml=/.jenkins/parallel_test_results.xml"
+make run_tests_parallel TEST_ARGS="${ARGS_FVDYN}"
 
 echo `ls -lh ${TEST_DATA_HOST}/*.txt`
 echo `cat ${TEST_DATA_HOST}/regression*.txt`
