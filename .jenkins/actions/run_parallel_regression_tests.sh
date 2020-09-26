80bd08574959b416857d5a53f6df0c17d4ab2933
Rhea George <rheag@vulcan.com>
2020-09-25 23:41:40 -0700

    add echo

--- .jenkins/actions/run_parallel_regression_tests.sh
+++ .jenkins/actions/run_parallel_regression_tests.sh
@@ -2,6 +2,7 @@
 set -e -x
 BACKEND=$1
 EXPNAME=$2
+echo 'TESTING SUBSET'
 ARGS="-vv -s -rsx --backend=${BACKEND} --which_modules=CubedToLatLon,DynCore,FVSubgridZ,HaloUpdate,HaloUpdate-2,HaloVectorUpdate,MPPBoundaryAdjust,MPPUpdateDomains,Tracer2D1L --junitxml=/.jenkins/parallel_test_results.xml"
 export EXPERIMENT=${EXPNAME}
 
@@ -13,7 +14,7 @@ make get_test_data
 
 # The default of this set to 1 causes a segfault
 make run_tests_parallel TEST_ARGS="${ARGS}"
-
+echo 'TESTING FVDynamics'
 ARGS_FVDYN="-vv -s -rsx --backend=${BACKEND} --which_modules=FVDynamics --junitxml=/.jenkins/parallel_test_results.xml"
 make run_tests_parallel TEST_ARGS="${ARGS_FVDYN}"
 
