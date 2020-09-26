846a48c9e259f7f6c2bedc9e9a82f7a28b16e752
Rhea George <rheag@vulcan.com>
2020-09-26 15:42:38 -0700

    loop over the tests

--- .jenkins/actions/run_parallel_regression_tests.sh
+++ .jenkins/actions/run_parallel_regression_tests.sh
@@ -3,7 +3,7 @@ set -e -x
 BACKEND=$1
 EXPNAME=$2
 echo 'TESTING SUBSET'
-ARGS="-vv -s -rsx --backend=${BACKEND} --which_modules=CubedToLatLon,DynCore,FVSubgridZ,HaloUpdate,HaloUpdate-2,HaloVectorUpdate,MPPBoundaryAdjust,MPPUpdateDomains,Tracer2D1L --junitxml=/.jenkins/parallel_test_results.xml"
+ARGS="-vv -s -rsx --backend=${BACKEND} --print_failures --which_modules=CubedToLatLon,DynCore,FVSubgridZ,HaloUpdate,HaloUpdate-2,HaloVectorUpdate,MPPBoundaryAdjust,MPPUpdateDomains,Tracer2D1L --junitxml=/.jenkins/parallel_test_results.xml"
 export EXPERIMENT=${EXPNAME}
 
 # Set the host data location
@@ -15,8 +15,11 @@ make get_test_data
 # The default of this set to 1 causes a segfault
 make run_tests_parallel TEST_ARGS="${ARGS}"
 echo 'TESTING FVDynamics'
-ARGS_FVDYN="-vv -s -rsx --backend=${BACKEND} --which_modules=FVDynamics --junitxml=/.jenkins/parallel_test_results.xml"
+ARGS_FVDYN="-vv -s -rsx --backend=${BACKEND}  --print_failures  --which_modules=FVDynamics --junitxml=/.jenkins/parallel_test_results.xml"
 make run_tests_parallel TEST_ARGS="${ARGS_FVDYN}"
-
+for COUNT in 1 2 3 4 5 
+do
+make run_tests_parallel TEST_ARGS="${ARGS_FVDYN} -vv -s -rsx --backend=${BACKEND}  --print_failures  "
+done
 echo `ls -lh ${TEST_DATA_HOST}/*.txt`
 echo `cat ${TEST_DATA_HOST}/regression*.txt`
