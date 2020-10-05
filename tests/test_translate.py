bf47ffc83fa415dbbb9988ea9a79ed1a96d59e42
Rhea George <rheag@vulcan.com>
2020-10-04 19:34:18 -0700

    it is sooooo annoying the barrier causes the failed parallel tests to hang

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -298,7 +298,7 @@ def test_parallel_savepoint(
     assert failing_names == [], f"only the following variables passed: {passing_names}"
     assert len(passing_names) > 0, f"No tests passed"
     sys.stdout.flush()
-    MPI.COMM_WORLD.Barrier()
+    #MPI.COMM_WORLD.Barrier()
 
 @contextlib.contextmanager
 def _subtest(failure_list, subtests, **kwargs):
