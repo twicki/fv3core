afe92088b4cd80cc51ff764cdac57ca2f884fa43
rheacangeo <rheag@vulcan.com>
2021-01-05 21:58:43 +0000

    print debugging info prior to slurm job launch

--- .jenkins/jenkins.sh
+++ .jenkins/jenkins.sh
@@ -105,6 +105,16 @@ if [ ! -z "${UPSTREAM_PROJECT}" ] ; then
 fi
 # If using sarus, load the image and set variables for running tests,
 # otherwise build the image
+echo "PRIOR TO JOB"
+echo $LD_LIBRARY_PATH
+echo $PATH
+echo `ls -lh /project/s1053/install/modulefiles/`
+echo `ls -lh /project/s1053/install/modulefiles/gcloud`
+module load daint-gpu
+module add "/project/s1053/install/modulefiles/"
+module load gcloud
+echo `which gsutil`
+echo "eeeks"
 if [ ${container_engine} == "sarus" ]; then
     module load sarus
     make sarus_load_tar
