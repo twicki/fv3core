db074546a9c3f5031a5b783b4f5c2461287a7f53
Eddie Davis <eddied@vulcan.com>
2021-01-18 14:44:19 -0800

    Build dataflow graph from stencil calsl

--- fv3core/stencils/pgradc.py
+++ fv3core/stencils/pgradc.py
@@ -1,13 +1,12 @@
+from typing import Any, Dict, Optional
+
 import gt4py.gtscript as gtscript
 from gt4py.gtscript import PARALLEL, computation, interval
 
 import fv3core._config as spec
 import fv3core.utils.gt4py_utils as utils
 from fv3core.decorators import gtstencil
-
-
-sd = utils.sd
-origin = utils.origin
+from fv3core.utils.typing import FloatField
 
 
 @gtscript.function
@@ -53,13 +52,13 @@ def p_grad_c_fn(uc_in, vc_in, delpc, pkc, gz, rdxc, rdyc, hydrostatic, dt2):
 
 @gtstencil()
 def p_grad_c(
-    uc_in: sd,
-    vc_in: sd,
-    delpc: sd,
-    pkc: sd,
-    gz: sd,
-    rdxc: sd,
-    rdyc: sd,
+    uc_in: FloatField,
+    vc_in: FloatField,
+    delpc: FloatField,
+    pkc: FloatField,
+    gz: FloatField,
+    rdxc: FloatField,
+    rdyc: FloatField,
     hydrostatic: int,
     dt2: float,
 ):
@@ -79,7 +78,14 @@ def p_grad_c(
 
 @gtstencil()
 def p_grad_c_ustencil(
-    uc_in: sd, delpc: sd, pkc: sd, gz: sd, rdxc: sd, *, hydrostatic: int, dt2: float
+    uc_in: FloatField,
+    delpc: FloatField,
+    pkc: FloatField,
+    gz: FloatField,
+    rdxc: FloatField,
+    *,
+    hydrostatic: int,
+    dt2: float,
 ):
     with computation(PARALLEL), interval(0, -1):
         uc_in = p_grad_c_u_wk(
@@ -89,7 +95,13 @@ def p_grad_c_ustencil(
 
 @gtstencil()
 def p_grad_c_vstencil(
-    vc_in: sd, delpc: sd, pkc: sd, gz: sd, rdyc: sd, hydrostatic: int, dt2: float
+    vc_in: FloatField,
+    delpc: FloatField,
+    pkc: FloatField,
+    gz: FloatField,
+    rdyc: FloatField,
+    hydrostatic: int,
+    dt2: float,
 ):
     with computation(PARALLEL), interval(0, -1):
         vc_in = p_grad_c_v_wk(
@@ -97,35 +109,30 @@ def p_grad_c_vstencil(
         )  # TODO: add [0, 0, 0] when gt4py bug is fixed
 
 
-def compute(uc, vc, delpc, pkc, gz, dt2):
-    # Options:
-    #      1) make a whole new storage for uc and vc out variables, paste
-    #         computed values into proper indices of uc_in and vc_in
-    #      2) copy the edges that aren't supposed to be computed for uc_in and
-    #         vc_in, operate on vc_in and uv_in, then paste in edges
-    #      3) compute uc and vc stencils separately specifying different domains
-    #         to compute on
-    grid = spec.grid
-    co = grid.compute_origin()
-    hydrostatic = int(spec.namelist.hydrostatic)
-    # Option 2
-    # uc_edge_i,  uc_edge_j, vc_edge_i, vc_edge_j = grid.edge_offset_halos(uc_in, vc_in)
-    # p_grad_c(uc_in, vc_in, delpc, pkc, gz, grid.rdxc, grid.rdyc,
-    #         hydrostatic=hydrostatic, dt2=dt2, origin=(grid.is_, grid.js, 0),
-    #         domain=(grid.nic+1, grid.njc + 1, grid.npz + 1))
-    # grid.append_edges(uc_in, uc_edge_i, uc_edge_j, vc_in, vc_edge_i, vc_edge_j)
+def compute(
+    uc: FloatField,
+    vc: FloatField,
+    delpc: FloatField,
+    pkc: FloatField,
+    gz: FloatField,
+    dt2: float,
+    **kwargs: Optional[Any],
+):
+    if "dfg" not in kwargs:
+        kwargs["dfg"] = dict()
 
-    # Option 3
+    grid = spec.grid
     p_grad_c_ustencil(
         uc,
         delpc,
         pkc,
         gz,
         grid.rdxc,
-        hydrostatic=hydrostatic,
+        hydrostatic=int(spec.namelist.hydrostatic),
         dt2=dt2,
-        origin=co,
+        origin=grid.compute_origin(),
         domain=(grid.nic + 1, grid.njc, grid.npz + 1),
+        **kwargs,
     )
     p_grad_c_vstencil(
         vc,
@@ -133,8 +140,11 @@ def compute(uc, vc, delpc, pkc, gz, dt2):
         pkc,
         gz,
         grid.rdyc,
-        hydrostatic=hydrostatic,
+        hydrostatic=int(spec.namelist.hydrostatic),
         dt2=dt2,
-        origin=co,
+        origin=grid.compute_origin(),
         domain=(grid.nic, grid.njc + 1, grid.npz + 1),
+        **kwargs,
     )
+    dfg: Dict[str, Set[Any]] = kwargs["dfg"]
+    stop=1
